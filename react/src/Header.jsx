2a294dd34feb872c3a582a42dacbc5d0e9775a2a
Chann04 <ChanShan04@gmail.com>
2025-11-20 08:19:19 +0800

    first commit

---
+++ react/src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./styles/appointments.css"
+
+function Header() {
+    return (
+        <nav className="fixed top-0 left-[270px] right-0 h-[70px] bg-[#6A3C3E] shadow-md z-40 flex items-center justify-end px-10">
+        <div className="text-white font-medium text-base tracking-wide">
+          Welcome back, Admin!
+        </div>
+      </nav>
+    );
+  }
+  
+  
+  export default Header;
+
